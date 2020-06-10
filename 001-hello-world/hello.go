96a1e2534445fc03764727eb046ac9b7f6d1266f
Phil Rowlands <phixlr@gmail.com>
2020-06-10 14:40:34 +0100

    noo

--- 001-hello-world/hello.go
+++ 001-hello-world/hello.go
@@ -5,3 +5,34 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+ /*cv
+// This is a bollock
+ */
+
+ fmt.Println("This iasass not a bollock")
+fmt.Println("This is not a bollock")
+fmt.Println("This is not a bollock")
+fmt.Println("This is not a bollock")
+fmt.Println("Thcvvis is not a bollock")
+fmt.Println("This is not a bollock")
+fmt.Println("This is  a bollock")
+fmt.Println("This is  a bollock")
+fmt.Println("This is  a bollock")
+fmt.Println("This is  a bollock")
+
+heh
+hehxcv
+heh
+hAehasa
+sdsA
+sdsdasaA
+asaA
+A
+sdsd
+sdsd
+dasa
+
+
+
+
